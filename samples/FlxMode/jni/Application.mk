ef3e5f935f1e8f496ea143902ddf28f707129f21
Dan V <dan@expandingbrain.com>
2012-05-16 14:22:19 +0800

    Fixed FlxMode sample.

--- samples/FlxMode/jni/Application.mk
+++ samples/FlxMode/jni/Application.mk
@@ -1,4 +1,5 @@
 APP_OPTIM := release
-APP_STL   := stlport_static
-# APP_ABI   := armeabi armeabi-v7a
+APP_STL   := gnustl_static
+APP_CPPFLAGS := -fexceptions -frtti
+APP_ABI   := armeabi armeabi-v7a
 
